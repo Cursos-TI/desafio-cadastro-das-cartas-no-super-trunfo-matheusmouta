e3f404a2e715f557c7a1719b97f55a2f4d1a948f
matheusmouta <matheussmoutasilva@gmail.com>
2025-05-09 20:12:04 +0000

    Desafio Nível Aventureiro Realizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@ int main(){
     printf("Desafio Super Trunfo!\n");
 
     // Criação Da Carta 1.
-    // Declarando variáveis e atribuindo os seus respectivos valores que estarão contidos nos dados da Carta 1.
+    // Declarando variáveis do Desafio Nível Novato e atribuindo os seus respectivos valores que estarão contidos nos dados da Carta 1.
     
     char estado1 = 'A';
     char codigo1[20] = "A01";
@@ -15,6 +15,9 @@ int main(){
     float area1 = 1521.11;
     float pib1 = 699.28;
     int pontos1 = 50;
+    float quocientedensi1 = populacao1 / area1;  // Variável referente ao Desafio Nível Aventureiro.
+    float quocientepib1 = pib1 / populacao1;     // Variável referente ao Desafio Nível Aventureiro.
+
     
     //Inserindo as funções printf e scanf para os dados da Carta 1.
 
@@ -39,6 +42,12 @@ int main(){
     printf("Pontos:\n ", pontos1);
     scanf("%d", &pontos1);
 
+    printf("Quociente Densidade 1: \n");
+    scanf("%f", &quocientedensi1);
+
+    printf("Quociente Pib1: \n");
+    scanf("%f", &quocientepib1);
+
     //Imprimindo os dados referentes à Carta 1.
     
     printf("Carta 1\nEstado: %c\n", estado1);
@@ -49,14 +58,20 @@ int main(){
     
     printf("População: %d\n", populacao1);
     
-    printf("Área: %f Km²\n", area1);
+    printf("Área: %.2f Km²\n", area1);
     
-    printf("PIB: %f bilhões de reais\n", pib1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
     
     printf("Número de Pontos Turísticos: %d\n", pontos1);
 
+    printf("Densidade Populacional: %.2f hab/Km²\n", quocientedensi1);
+
+    printf("PIB per Capita: %.2f reais\n", quocientepib1);
+
+
+
     // Criando a Carta 2.
-    // Declarando variáveis e atribuindo seus respectivos valores que estarão contidos nos dados da Carta 2.
+    // Declarando variáveis do Desafio Nível Novato e atribuindo seus respectivos valores que estarão contidos nos dados da Carta 2.
     
     char estado2 = 'B';
     char codigo2[20] = "B02";
@@ -65,6 +80,9 @@ int main(){
     float area2 = 1200.25;
     float pib2 = 300.50;
     int pontos2 = 30;
+    float quocientedensi2 = populacao2 / area2;  // Variável referente ao Desafio Nível Aventureiro.
+    float quocientepib2 = pib2 / populacao2;     // Variável referente ao Desafio Nível Aventureiro.
+
         
     // Inserindo as funções printf e scanf para Carta 2.
     
@@ -89,6 +107,12 @@ int main(){
     printf("Números de Pontos Turísticos: \n", pontos2);
     scanf("%d", &pontos2);
 
+    printf("Quociente Densidade 2: \n");
+    scanf("%f", &quocientedensi2);
+
+    printf("Quociente Pib2: \n");
+    scanf("%f", &quocientepib2);
+
     //Imprimindo os dados referentes à Carta 2.
 
     printf("Carta 2\nEstado: B%c\n", estado2);
@@ -99,12 +123,17 @@ int main(){
 
     printf("População: %d\n", populacao2);
 
-    printf("Área: %f Km²\n", area2);
+    printf("Área: %.2f Km²\n", area2);
 
-    printf("PIB: %f bilhões de reais\n", pib2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
 
     printf("Número de Pontos Turísticos: %d\n", pontos2);
-    
+
+    printf("Densidade Populacional: %.2f hab/Km²\n", quocientedensi2);
+
+    printf("PIB per Capita: %.2f reais\n", quocientepib2);
+
+
     return 0;
 
 }
